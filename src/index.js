5d3bc11e8357aa4232c4fc4289503c3d487491f8
Rishikesh Joshi <rjjoshixyz@gmail.com>
2020-10-27 15:16:11 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import ColoredRect from "./App.js";
+
+ReactDOM.render(<ColoredRect />, document.getElementById("root"));
